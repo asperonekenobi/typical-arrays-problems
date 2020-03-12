a5eef30d294d852ee136bc516a1fec13b81da2f6
AndreySinkovets <andreysinkovets@yahoo.com>
2020-03-12 23:11:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+      return 0;
+  } else {
+      let min = array[0];
+      for (let i = 0; i < array.length; i++) {
+          if (array[i] < min) {
+              min = array[i];
+          }
+      }
+      return min;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+      return 0;
+  } else {
+      let max = array[0];
+      array.forEach(element => {
+          if (element > max) {
+              max = element;
+          }
+      });
+      return max;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+      return 0;
+  } else {
+      let sum = array.reduce((acc,item) => acc+item);
+      return sum/array.length;
+  }
 }
